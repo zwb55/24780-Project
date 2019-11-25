48f5177fe2ddcc3105a7ebf4f2233ac3b92f0573
jamie-yjh <57369446+jamie-yjh@users.noreply.github.com>
2019-11-25 10:38:32 -0500

    Start Scene

--- PuzzleGame/PuzzleGame_main.cpp
+++ PuzzleGame/PuzzleGame_main.cpp
@@ -18,6 +18,12 @@ For test
 
 const int MAX_LEVEL = 8;
 
+/* the 2 png using in start scene*/
+YsRawPngDecoder png[2];
+GLuint texId[2];
+void init_pngs();
+void drawStartScene(JokermanFont& jokerman, double& theta, double& delta);
+
 /*
 Define the main logic and display during one game, return an int code according
 to different end state of the game:
@@ -167,7 +173,14 @@ Press ESC to go back to main window
 int main(void) {
 	srand(time(NULL));
 	bool terminate = false;
-	int currLevel = 1;
+	int currLevel = 7;
+
+	int leftButton, middleButton, rightButton, locX, locY;
+	int mouseEvent;
+
+	double theta = 0.;  // for the startscene
+	double delta = 0.5;
+
 	Camera3D camera;
 	OrbitingViewer orbit;
 	Controller* gameController;
@@ -175,13 +188,20 @@ int main(void) {
 
 	FsOpenWindow(16, 16, 1200, 800, 1);
 
+	JokermanFont jokerman;
+	init_pngs();
+
 	while (!terminate) {
 		int key = FsInkey();
+		mouseEvent = FsGetMouseEvent(leftButton, middleButton, rightButton, locX, locY);
 
 		if (key == FSKEY_ESC) {
 			terminate = true;
 		}
-		else if (key == FSKEY_S || returnCode != 0) {
+		else if ((mouseEvent == FSMOUSEEVENT_LBUTTONDOWN &&
+			locX > 430 && locX < 710 &&
+			locY > 620 && locY < 700)
+			|| returnCode != 0) {
 			// start a new game
 			gameController = new Controller(currLevel);
 			returnCode = gamePlay(camera, orbit, gameController);
@@ -200,16 +220,94 @@ int main(void) {
 		else if (returnCode == 0) {
 			// reset to the first level of the game
 			currLevel = 1;
+			// start scene from here
+			drawStartScene(jokerman, theta, delta);
+			FsSwapBuffers();
+			FsSleep(10);
 		}
 
 		// show main window
 		FsPollDevice();
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		glColor3ub(255, 0, 0);
+		/*glColor3ub(255, 0, 0);
 		DrawingUtilNG::drawRectangle(0, 0, 800, 600, true);
 
 		FsSwapBuffers();
-		FsSleep(10);
+		FsSleep(10);*/
 	}
+}
+
+void init_pngs()
+{
+	png[0].Decode("startscene.png");
+	png[1].Decode("scrshot.png");
+	for (int i = 0; i < 2; i++)
+	{
+		glGenTextures(1, &texId[i]);  // Reserve one texture identifier
+		glBindTexture(GL_TEXTURE_2D, texId[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexImage2D
+		(GL_TEXTURE_2D,
+			0,    // Level of detail
+			GL_RGBA,
+			png[i].wid,
+			png[i].hei,
+			0,    // Border width, but not supported and needs to be 0.
+			GL_RGBA,
+			GL_UNSIGNED_BYTE,
+			png[i].rgba);
+	}
+}
+
+void drawStartScene(JokermanFont& jokerman, double& theta, double& delta)
+{
+	int wid, hei;
+	FsGetWindowSize(wid, hei);
+
+	glViewport(0, 0, wid, hei);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, (float)wid - 1, (float)hei - 1, 0, -1, 1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glColor4d(1.0, 1.0, 1.0, 1.0);   // this color will "tint" the image
+
+	// enable texture mapping
+	glEnable(GL_TEXTURE_2D);
+
+	// bind a texture to OpenGL primitives
+	glBindTexture(GL_TEXTURE_2D, texId[0]);
+
+	glBegin(GL_QUADS);
+
+	glTexCoord2d(0.0, 0.0);
+	glVertex2i(0, 0);
+	glTexCoord2d(1.0, 0.0);
+	glVertex2i(1200, 0);
+	glTexCoord2d(1.0, 1.0);
+	glVertex2i(1200, 800);
+	glTexCoord2d(0.0, 1.0);
+	glVertex2i(0, 800);
+
+	glEnd();
+
+	// turn off texture 
+	glDisable(GL_TEXTURE_2D);
+
+	// draw text start button
+	theta += delta;
+	if (theta > 10. || theta < -10.) delta *= -1;
+
+	jokerman.setColorHSV(210, 1, 1);
+	jokerman.drawText("MINI  MAZE", 230, 264, 1.2, 0);
+	jokerman.setColorHSV(0, 0, 100);
+	jokerman.drawText("START!", 430, 720, 0.8, theta);
 }
